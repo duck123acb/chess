e95813c6095d9587db27545b75592378ab30e7ac
Danush Ramanan <dan.ramanan@gmail.com>
2024-06-26 16:51:56 -0600

    player movement

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,48 @@
 use macroquad::prelude::*;
 
 struct Paddle {
-  pub x: f32,
-  pub y: f32,
-  pub width: f32,
-  pub height: f32
+  x: f32,
+  y: f32,
+  width: f32,
+  height: f32,
+  speed: f32,
+  up_key: KeyCode,
+  down_key: KeyCode
 }
-
 impl Paddle {
-  fn new(paddle_x: f32, paddle_y: f32) -> Paddle {
-    Paddle {
+  fn new(paddle_x: f32, paddle_y: f32, paddle_up_key: KeyCode, paddle_down_key: KeyCode) -> Self {
+    Self {
       x: paddle_x,
       y: paddle_y,
       width: 25.0,
-      height: 200.0
+      height: 200.0,
+      speed: 700.0,
+      up_key: paddle_up_key,
+      down_key: paddle_down_key
     }
   }
   fn draw(&self) {
-    draw_rectangle(self.x, self.y, self.width, self.height, WHITE);
+    draw_rectangle(self.x - (self.width / 2.0), self.y - (self.height / 2.0), self.width, self.height, WHITE);
   }
+  fn update(&mut self, dt: f32) {
+    let dir;
+
+    if is_key_down(self.up_key) && is_key_down(self.down_key) {
+      dir = 0.0; // if both keys are down, dont move
+    }
+    else if is_key_down(self.up_key) {
+      dir = -1.0;
+    }
+    else if is_key_down(self.down_key) {
+      dir = 1.0;
+    }
+    else {
+      dir = 0.0
+    }
+
+    self.y += self.speed * dir * dt;
+  }
+
 }
 
 fn window_conf() -> Conf {
@@ -32,11 +56,19 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
-  let paddle = Paddle::new(0.0, 0.0);
+  let mut paddle_left = Paddle::new(40.0, screen_height() / 2.0, KeyCode::W, KeyCode::S);
+  let mut paddle_right = Paddle::new(screen_width() - 40.0, screen_height() / 2.0, KeyCode::I, KeyCode::K);
+  
   loop {
+    /* LOGIC */
+    paddle_left.update(get_frame_time());
+    paddle_right.update(get_frame_time());
+
+    /* RENDERING */
     clear_background(BLACK);
 
-    paddle.draw();
+    paddle_left.draw();
+    paddle_right.draw();
 
     next_frame().await
   }
