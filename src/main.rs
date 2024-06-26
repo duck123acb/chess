918558e3aa6c41ad047e5415f638e016c6756722
Danush Ramanan <dan.ramanan@gmail.com>
2024-06-26 16:23:37 -0600

    add paddle object

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,43 @@
 use macroquad::prelude::*;
 
+struct Paddle {
+  pub x: f32,
+  pub y: f32,
+  pub width: f32,
+  pub height: f32
+}
 
-#[macroquad::main("chess")]
+impl Paddle {
+  fn new(paddle_x: f32, paddle_y: f32) -> Paddle {
+    Paddle {
+      x: paddle_x,
+      y: paddle_y,
+      width: 25.0,
+      height: 200.0
+    }
+  }
+  fn draw(&self) {
+    draw_rectangle(self.x, self.y, self.width, self.height, WHITE);
+  }
+}
+
+fn window_conf() -> Conf {
+  Conf {
+    window_title: "chess".to_string(),
+    window_width: 800,
+    window_height: 600,
+    ..Default::default()
+  }
+}
+
+#[macroquad::main(window_conf)]
 async fn main() {
+  let paddle = Paddle::new(0.0, 0.0);
   loop {
+    clear_background(BLACK);
+
+    paddle.draw();
+
     next_frame().await
   }
 }
