8c601a38a1482196e6e55956e8d1c93eabca5a78
Danush Ramanan <dan.ramanan@gmail.com>
2024-06-26 22:00:32 -0600

    getting things to draw from an atlas

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use macroquad::prelude::*;
+use macroquad::{prelude::*, texture};
 
 fn window_conf() -> Conf {
   Conf {
@@ -10,16 +10,52 @@ fn window_conf() -> Conf {
   }
 }
 
+struct Piece {
+
+}
+struct Square {
+  rect: Rect,
+  piece: Piece
+}
+
+fn draw_from_atlas(
+  texture: &Texture2D,
+  x: i32,
+  y: i32,
+  width: i32,
+  height: i32,
+  dest_x: f32,
+  dest_y: f32,
+) {
+  let params = DrawTextureParams {
+      dest_size: Some(vec2(width as f32, height as f32)),
+      source: Some(Rect {
+          x: x as f32,
+          y: y as f32,
+          w: width as f32,
+          h: height as f32,
+      }),
+      ..Default::default()
+  };
+
+  draw_texture_ex(texture, dest_x, dest_y, WHITE, params);
+}
+
 #[macroquad::main(window_conf)]
 async fn main() {
-  
+  let texture_atlas = load_texture("assets/pieces.png").await.unwrap();
+  texture_atlas.set_filter(FilterMode::Nearest);
+
+
+
   loop {
     /* LOGIC */
-
+    
 
     /* RENDERING */
     clear_background(BLACK);
-
+    
+    draw_from_atlas(&texture_atlas, 0, 0, 130, 130, 100.0, 100.0); // Draw the first sprite
 
     next_frame().await
   }
