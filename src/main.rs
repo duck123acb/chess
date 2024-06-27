62baf04b787f32c1b09d00d22f04002af5ca578c
Danush Ramanan <dan.ramanan@gmail.com>
2024-06-27 06:37:55 -0600

    basic rendering logic for the pieces, also updated the draw_from_atlas function

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,41 @@ fn window_conf() -> Conf {
     ..Default::default()
   }
 }
-
-struct Piece {
-
-}
-struct Square {
-  rect: Rect,
-  piece: Piece
-}
-
-fn draw_from_atlas(texture: &Texture2D, texture_x: i32, texture_y: i32, width: i32, height: i32, dest_x: f32, dest_y: f32) {
+fn draw_from_atlas(texture: &Texture2D, texture_x: i32, texture_y: i32, texture_rect: Rect) { // macroquad doesn't have a built in function for drawing from an atlas
   let params = DrawTextureParams {
-    dest_size: Some(vec2(width as f32, height as f32)),
-    source: Some(Rect {x: texture_x as f32, y: texture_y as f32, w: width as f32, h: height as f32}),
+    dest_size: Some(vec2(texture_rect.w, texture_rect.h)),
+    source: Some(Rect {x: texture_x as f32, y: texture_y as f32, w: texture_rect.w, h: texture_rect.h}),
     ..Default::default()
   };
 
-  draw_texture_ex(texture, dest_x, dest_y, WHITE, params);
+  draw_texture_ex(texture, texture_rect.x, texture_rect.y, WHITE, params);
+}
+
+struct PieceSprite {
+  rect: Rect,
+  texture: Texture2D,
+  piece_type: char // change to the dedicated piece enum later
+}
+impl PieceSprite {
+  fn new(sprite_x: i32, sprite_y: i32, sprite_texture: Texture2D, sprite_type: char) -> Self {
+    Self {
+      rect: Rect::new(sprite_x as f32, sprite_y as f32, 130f32, 130f32),
+      texture: sprite_texture,
+      piece_type: sprite_type
+    }
+  }
+
+  fn update(&mut self) {
+
+  }
+  fn draw(&self) {
+    // depending on type, update the texture_x and texture_y
+    draw_from_atlas(&self.texture, 0, 0, self.rect);
+  }
+}
+struct Square {
+  rect: Rect,
+  piece: PieceSprite
 }
 
 #[macroquad::main(window_conf)]
@@ -33,17 +51,12 @@ async fn main() {
   let texture_atlas = load_texture("assets/pieces.png").await.unwrap();
   texture_atlas.set_filter(FilterMode::Nearest);
 
-
-
   loop {
     /* LOGIC */
-    
 
     /* RENDERING */
     clear_background(BLACK);
     
-    draw_from_atlas(&texture_atlas, 0, 0, 130, 130, 0.0, 0.0); // Draw the first sprite
-
     next_frame().await
   }
 }
