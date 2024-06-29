7eea20eda4836d39bef35a338cdbf2cc0db83549
Danush Ramanan <dan.ramanan@gmail.com>
2024-06-29 11:23:34 -0600

    make basic board structure

--- src/board.rs
+++ src/board.rs
@@ -1,2 +1,14 @@
+use crate::utils::PieceType;
+
 struct Board {
+  bitboards: [u64; 12],
+  // add other flags when needed
+}
+impl Board {
+  fn parse_fen(&mut self) {
+
+  }
+  fn export() { // return board as an array for the renderer to draw
+    
+  }
 }
