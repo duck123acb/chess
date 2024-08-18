3b5577f55e3879a2a49b8534aabf486fde10e58e
Danush Ramanan <dan.ramanan@gmail.com>
2024-08-17 22:31:39 -0600

    oops

--- src/bot/mod.rs
+++ src/bot/mod.rs
@@ -25,9 +25,9 @@ fn evaluate_position(board: &Board) -> i32 {
   0 // lmaooooo best evaluation
 }
 
-fn minimax(board: &mut Board, move_to_search: Move, depth: i32, alpha: &mut i32, beta: &mut i32, maximizing_player: bool) -> EvalMove { // thanks to Sebastian Lague!! https://www.youtube.com/watch?v=l-hh51ncgDI
+fn minimax(board: &mut Board, move_to_search: Move, depth: i32, alpha: &mut i32, beta: &mut i32, maximizing_player: bool) -> EvalMove { 
   if depth == 0 || board.is_game_over() {
-    return EvalMove::new(move_to_search, evaluate_position(board))
+    return EvalMove::new(move_to_search, evaluate_position(board));
   }
 
   if maximizing_player {
@@ -37,17 +37,14 @@ fn minimax(board: &mut Board, move_to_search: Move, depth: i32, alpha: &mut i32,
       board.make_move(piece_move);
 
       let eval_move = minimax(board, piece_move, depth - 1, alpha, beta, false);
-      let better_eval = if max_eval.eval <= eval_move.eval {
-        eval_move
-      } else {
-        max_eval
-      };
-      max_eval = better_eval;
+      if eval_move.eval > max_eval.eval {
+        max_eval = EvalMove::new(piece_move, eval_move.eval);
+      }
 
       board.undo_move(piece_move);
 
-      *alpha = cmp::max(*alpha, eval_move.eval);
-      if beta <= alpha {
+      *alpha = cmp::max(*alpha, max_eval.eval);
+      if *beta <= *alpha {
         break;
       }
     }
@@ -61,24 +58,22 @@ fn minimax(board: &mut Board, move_to_search: Move, depth: i32, alpha: &mut i32,
       board.make_move(piece_move);
 
       let eval_move = minimax(board, piece_move, depth - 1, alpha, beta, true);
-      let better_eval = if min_eval.eval >= eval_move.eval {
-        eval_move
-      } else {
-        min_eval
-      };
-      min_eval = better_eval;
+      if eval_move.eval < min_eval.eval {
+        min_eval = EvalMove::new(piece_move, eval_move.eval);
+      }
 
       board.undo_move(piece_move);
       
-      *beta = cmp::min(*beta, eval_move.eval);
-      if beta <= alpha {
+      *beta = cmp::min(*beta, min_eval.eval);
+      if *beta <= *alpha {
         break;
       }
     }
-    
+
     return min_eval;
   }
 }
+
 
 pub struct Bot {
   is_white_player: bool
@@ -92,7 +87,7 @@ impl Bot {
 
   pub fn get_best_move(&self, board: &mut Board) -> Move {
     let moves = board.get_all_moves();
-    let best_move = minimax(board, moves[0], 3, &mut NEGATIVE_INIFINITY, &mut INIFINITY, self.is_white_player);
+    let best_move = minimax(board, moves[0], 5, &mut NEGATIVE_INIFINITY, &mut INIFINITY, self.is_white_player);
     // println!("{:b}", (1u64 << best_move.board_move.start_square) | (1u64 << best_move.board_move.end_square));
     best_move.board_move
     // moves[moves.len() - 1]
