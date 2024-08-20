aa21a6149fd9d15fe2601dfc38b2ee7dc39ad682
Danush Ramanan <dan.ramanan@gmail.com>
2024-08-19 21:45:35 -0600

    making a new move each itteration seems to be the meta cause it might get overwritten later on

--- src/bot/mod.rs
+++ src/bot/mod.rs
@@ -9,72 +9,70 @@ use evaluation::*;
 const INIFINITY: i32 = i32::MAX;
 const NEGATIVE_INIFINITY: i32 = i32::MIN;
 
+fn minimax(board: Board, depth: i32, mut alpha: i32, mut beta: i32, maximizing_player: bool) -> (i32, Option<Move>) { 
+  if depth == 0 || board.is_checkmate() {
+    return (evaluate_position(board), None);
+  }
+
+  let mut best_move: Option<Move> = None;
+
+  if maximizing_player {
+    let mut max_eval = NEGATIVE_INIFINITY;
+
+    for piece_move in board.get_all_moves() {
+      let mut iteration_board = board.clone();
+      iteration_board.make_move(piece_move);
+
+      let (eval, _) = minimax(iteration_board, depth - 1, alpha, beta, false);
+      if eval > max_eval {
+        max_eval = eval;
+        best_move = Some(piece_move);
+      }
+
+      alpha = cmp::max(alpha, eval);
+      if beta <= alpha {
+        break;
+      }
+    }
+
+    return (max_eval, best_move);
+  }
+  else {
+    let mut min_eval = INIFINITY;
+
+    for piece_move in board.get_all_moves() {
+      let mut iteration_board = board.clone();
+      iteration_board.make_move(piece_move);
+
+      let (eval, _) = minimax(iteration_board, depth - 1, alpha, beta, true);
+      if eval < min_eval {
+        min_eval = eval;
+        best_move = Some(piece_move);
+      }
+
+      beta = cmp::min(beta, eval);
+      if beta <= alpha {
+        break;
+      }
+    }
+
+    return (min_eval, best_move);
+  }
+}
+
+
 pub struct Bot {
-  is_white_player: bool,
-  best_move: Move
+  is_white_player: bool
 }
 impl Bot {
   pub fn new(is_white: bool) -> Self {
     Self {
       is_white_player: is_white,
-      best_move: Move::default()
-    }
-  }
-
-  fn minimax(&mut self, board: Board, depth: i32, alpha: &mut i32, beta: &mut i32, maximizing_player: bool) -> i32 { 
-    if depth == 0 || board.is_checkmate() {
-      return evaluate_position(board);
-    }
-  
-    if maximizing_player {
-      let mut max_eval = NEGATIVE_INIFINITY;
-  
-      for piece_move in board.get_all_moves() {
-        let mut itteration_board = board.clone();
-        itteration_board.make_move(piece_move);
-  
-        let eval = self.minimax(itteration_board, depth - 1, alpha, beta, false);
-        if eval >= max_eval {
-          max_eval = eval;
-          self.best_move = piece_move;
-        }
-  
-        *alpha = cmp::max(*alpha, max_eval);
-        if *beta <= *alpha {
-          break;
-        }
-      }
-  
-      return max_eval;
-    }
-    else {
-      let mut min_eval = INIFINITY;
-  
-      for piece_move in board.get_all_moves() {
-        let mut itteration_board = board.clone();
-        itteration_board.make_move(piece_move);
-  
-        let eval = self.minimax(itteration_board, depth - 1, alpha, beta, true);
-        if eval <= min_eval {
-          min_eval = eval;
-          self.best_move = piece_move;
-        }
-  
-        *beta = cmp::min(*beta, min_eval);
-        if *beta <= *alpha {
-          break;
-        }
-      }
-  
-      return min_eval;
     }
   }
 
   pub fn get_best_move(&mut self, board: Board) -> Move {
-    let mut alpha = NEGATIVE_INIFINITY;
-    let mut beta = INIFINITY;
-    
-    self.minimax(board, 3, &mut alpha, &mut beta, self.is_white_player); // the initial move passed in here doesnt matter
-    self.best_move
+    let (_score, best_move) = minimax(board, 3, NEGATIVE_INIFINITY, INIFINITY, self.is_white_player);
+    best_move.unwrap()
   }
 }
